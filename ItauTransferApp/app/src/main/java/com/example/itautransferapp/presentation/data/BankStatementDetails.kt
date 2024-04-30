0048ccb731ee99d843722886a29eebf5c4f7624f
Lise <35637366+luararamos@users.noreply.github.com>
2024-04-30 17:17:55 -0300

    Creating BankStatemetDetails

---
+++ ItauTransferApp/app/src/main/java/com/example/itautransferapp/presentation/data/BankStatementDetails.kt
@@ -0,0 +1,8 @@
+package com.example.itautransferapp.presentation.data
+
+data class BankStatementDetails (
+    val typeTransfer: String,
+    val name: String,
+    val value: String,
+    val date: String
+)
---
+++ ItauTransferApp/app/src/main/java/com/example/itautransferapp/presentation/screens/listStatement/ListBankStatementViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.itautransferapp.presentation.screens.listStatement
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.example.itautransferapp.domain.repository.UserRepository
+
+class ListBankStatementViewModel (
+    private val applicationContext: Context,
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+
+}
